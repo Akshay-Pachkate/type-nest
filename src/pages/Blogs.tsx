d1d044dfe6ed3296431976256846ec36b013b328
Akshay Pachkate <akshaypachkate23@gmail.com>
2025-04-14 23:38:59 +0530

    login check

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,12 +1,22 @@
+import { useEffect} from "react";
 import AppBar from "../components/Appbar";
 import BlogCard from "../components/BlogCard"
 import useBlogs from "../hooks/bulk"
 import BlogsSkeleton from "../skeletons/BlogsSkeleton";
+import { useNavigate } from "react-router-dom";
 
 
 const Blogs = () => {
 
   const {blogs, loading, error} = useBlogs();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const jwt = localStorage.getItem('jwt');
+    if (!jwt) {
+      navigate('/signin');
+    } 
+  }, []);
 
   if(error)
     return <div className="w-[48%] mx-auto">{error}</div>
